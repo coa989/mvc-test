1d6d93d8e2ee2841f743867d394a3b1590c1cd74
coa989 <a.marjanovic989@gmail.com>
2021-05-11 22:22:08 +0200

    added pdo connection

--- core/Database.php
+++ core/Database.php
@@ -10,6 +10,9 @@ class Database
 
     public function __construct()
     {
+        $dsn = $_ENV['DB_DSN'];
+        $user = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASSWORD'];
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
