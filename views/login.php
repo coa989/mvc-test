7ae7e331ef55ae943d85967e24db29a20f05cddc
coa989 <a.marjanovic989@gmail.com>
2021-05-11 12:21:16 +0200

    added markup

--- views/login.php
+++ views/login.php
@@ -1,1 +1,15 @@
 <h1>Login</h1>
+<form action="" method="post">
+    <div class="form-group">
+        <label>Email</label>
+        <input type="text" name="email" class="form-control">
+        <span class="invalid-feedback"></span>
+    </div>
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="password" class="form-control" value="">
+        <span class="invalid-feedback"></span>
+    </div>
+
+    <button type="submit" class="btn btn-primary mt-2">Submit</button>
+</form>
--- views/register.php
+++ views/register.php
@@ -1,1 +1,25 @@
 <h1>Register</h1>
+<form action="" method="post">
+    <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="username" class="form-control">
+        <span class="invalid-feedback"></span>
+    </div>
+    <div class="form-group">
+        <label>Email</label>
+        <input type="text" name="email" class="form-control">
+        <span class="invalid-feedback"></span>
+    </div>
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="password" class="form-control" value="">
+        <span class="invalid-feedback"></span>
+    </div>
+    <div class="form-group">
+        <label>Confirm Password</label>
+        <input type="password" name="confirmPassword" class="form-control" value="">
+        <span class="invalid-feedback"></span>
+    </div>
+
+    <button type="submit" class="btn btn-primary mt-2">Submit</button>
+</form>
