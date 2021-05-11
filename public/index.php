0b5e9ccf6ddbbb07f263aef902cf155fc2aa6086
coa989 <a.marjanovic989@gmail.com>
2021-05-11 22:33:06 +0200

    added post route for register and login

--- public/index.php
+++ public/index.php
@@ -6,10 +6,16 @@ use app\controllers\AuthController;
 use app\controllers\SiteController;
 use app\core\Application;
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', [SiteController::class, 'home']);
+
 $app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
 $app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
