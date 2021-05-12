6c33344f8f59282b14e4bf9aa401a937cfacfb74
coa989 <a.marjanovic989@gmail.com>
2021-05-12 23:41:16 +0200

    Implemented register method

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -5,16 +5,32 @@ namespace app\controllers;
 
 
 use app\core\Controller;
+use app\core\Request;
+use app\models\User;
 
 class AuthController extends Controller
 {
-    public function login()
+
+    public function register(Request $request)
     {
+        $user = new User();
+        if ($request->isPost()) {
+            $user->loadData($request->getBody());
+            if ($user->validate() && $user->save()) {
+                return 'Success';
+            }
+        }
+        return $this->render('register', [
+            'model' => $user
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if ($request->isPost()) {
+
+        }
         return $this->render('login');
     }
 
-    public function register()
-    {
-        return $this->render('register');
-    }
 }
