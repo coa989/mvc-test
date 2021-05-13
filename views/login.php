1a44a03cc97514151fe2c6cbd2df9e89ecbbfb66
coa989 <a.marjanovic989@gmail.com>
2021-05-13 16:36:20 +0200

    Added login errors in view

--- views/login.php
+++ views/login.php
@@ -1,14 +1,15 @@
+<?php /** @var $model \app\models\User */ ?>
 <h1>Login</h1>
 <form action="" method="post">
     <div class="form-group">
         <label>Email</label>
-        <input type="text" name="email" class="form-control">
-        <span class="invalid-feedback"></span>
+        <input type="text" name="email" class="form-control <?= $model->errors['email'] ? 'is-invalid' : '' ?>" value="<?= $model->email ?? '' ?>">
+        <span class="invalid-feedback"><?= $model->errors['email'] ?></span>
     </div>
     <div class="form-group">
         <label>Password</label>
-        <input type="password" name="password" class="form-control" value="">
-        <span class="invalid-feedback"></span>
+        <input type="password" name="password" class="form-control <?= $model->errors['password'] ? 'is-invalid' : '' ?>" value="">
+        <span class="invalid-feedback"><?= $model->errors['password'] ?></span>
     </div>
 
     <button type="submit" class="btn btn-primary mt-2">Submit</button>
